cf0268900fa052b3835763a3b65bf3853f312e6b
Jaumm111 <jvmveiga@gmail.com>
2024-06-06 16:48:16 -0300

    base

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+class Jogador {
+private:
+    string nome;
+    int ordem;
+    int dinheiro;
+    int posicao_tab;
+    int posicao_casa;
+    bool preso;
+
+public:
+    Jogador() {
+    }
+    
+};
+
+
+
+int main() {
+
+
+};
