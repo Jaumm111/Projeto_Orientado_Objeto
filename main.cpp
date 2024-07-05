cd11200069f0f6b37e8834d820d9d598ac1bfe4c
Jaumm111 <56032067+Jaumm111@users.noreply.github.com>
2024-07-05 09:14:20 -0300

    iniciando_classes

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <string>
 #include <cmath>
+#include <vector>
+#include <QApplication>
+#include <QtWidgets>
 
 using namespace std;
 
@@ -9,17 +12,79 @@ private:
     string nome;
     int ordem;
     int dinheiro;
+    int id;
     int posicao_tab;
     int posicao_casa;
     bool preso;
-
+    bool sua_vez;
+    vector<Item *> iventario;
 public:
     Jogador() {
     }
-    
+    void jogar_dados();
+    void mover_peao(int num);
+    void receber_dinheiro(int num);
+    void pagar(int num,int id_jog = 0);
+    void add_item();
+
 };
 
+void Jogador::jogar_dados(){
 
+}
+
+void Jogador::mover_peao(){
+
+}
+
+void Jogador::receber_dinheiro(){
+
+}
+
+void Jogador::pagar(int num, int id_jog=0){
+
+}
+
+class Item{
+private:
+    string tipo;
+public:
+    void usar();
+}
+class I_Prop: public Item{
+private:
+    string tipo="propriedade";
+public:
+}
+class I_
+
+class Tabuleiro{
+private:
+    int id;
+    vector<Casa *> 
+}
+
+class Casa{
+private:
+    int id;
+public:
+    void Cair{
+
+    }
+
+}
+class Propriedade: public Casa{
+
+}
+class Sorte_Reves: public Casa{
+
+}
+
+class Baralho{
+public:
+    vector<Carta *> cartas;
+    int pos;
+}
 
 int main() {
 
