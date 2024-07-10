bdb3e22766dc1aaf1f3e58277d1766cd025c6e26
Jaumm111 <jvmveiga@gmail.com>
2024-07-10 08:35:15 -0300

    tabuleiro

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <cmath>
 #include <vector>
+#include <map>
+
 #include <QApplication>
 #include <QtWidgets>
 
@@ -49,35 +51,70 @@ class Item{
 private:
     string tipo;
 public:
-    void usar();
+    void clicar();
 }
 class I_Prop: public Item{
 private:
     string tipo="propriedade";
 public:
+    void clicar();
 }
-class I_
+class I_Carta: public Item{
+private:
+    string tipo="carta";
+public:
+    void clicar();
+}
+class I_Din: public Item{
+private:
+    string tipo="din";
+public:
+    void clicar();
+}
 
 class Tabuleiro{
 private:
     int id;
-    vector<Casa *> 
+    vector<Casa> casas;
+    map<string, pair<int,int>>  status;
+public:
+    Tabuleiro(vector<Casa> casas_){
+        casas = casas_;
+        for(unsigned i=0;i <  casas.sizr(); i++){
+            if (casas[i].tipo() == Casa::CasaType::PROPRIEDADE){
+                status[casas[i].get_Cor()].first = 1 + status[casas[i].get_Cor()].first;
+            }
+        }
+
+
+    }
 }
 
 class Casa{
 private:
     int id;
 public:
+    enum {
+        PROPRIEDADE,
+        SORTE_REVES
+    } CasaType;
+
     void Cair{
 
     }
+    virtual Casa::CasaType tipo() = 0;
 
 }
 class Propriedade: public Casa{
+    public:
+
+    CasaType tipo() { return CasaType::PROPRIEDADE; }
 
 }
 class Sorte_Reves: public Casa{
 
+public:
+    CasaType tipo() { return CasaType::SORTE_REVES; }
 }
 
 class Baralho{
